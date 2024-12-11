cc78380267901fcb6568202de34f18bce1302f92
DeepakShinde123 <Deepakshinde123@proton.me>
2024-12-11 05:26:05 +0000

    aside done

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,38 +1,59 @@
-import { TbMessageCircle,TbMap2, TbDeviceMobile, TbBrandFacebook,TbBrandLinkedin,TbBrandX } from "react-icons/tb";
-function Aside(){
+import {
+  TbMessageCircle,
+  TbMap2,
+  TbDeviceMobile,
+  TbBrandFacebook,
+  TbBrandLinkedin,
+  TbBrandX,
+} from "react-icons/tb";
+
+function Aside() {
   const contactInfo = [
     {
-      id:0,
-      icon: <TbMessageCircle />,
-      tilte:"Chat to us",
-      subTitle: "Our friendly Team",
-      action: "hi@untitled.com",
+      id: 0,
+      icon: TbMessageCircle,
+      title: "Chat to us",
+      description: "Our friendly team",
+      action: "hi@untitledui.com",
     },
     {
-      id:0,
-      icon: <TbMessageCircle />,
-      tilte:"Visit us",
-      subTitle: "Come say hello at our office HQ",
-      action: "100 smith street Collingwood VIC 3000 AU",
+      id: 1,
+      icon: TbMap2,
+      title: "Visit us",
+      description: "Come say hello at our office HQ",
+      action: "100 Smith Street Collingwood VIC 3066 AU",
     },
     {
-      id:0,
-      icon: <TbMessageCircle />,
-      tilte:"Call us",
-      subTitle: "Mon-fri from 8am-5pm",
+      id: 2,
+      icon: TbDeviceMobile,
+      title: "Call us",
+      description: "Mon-Fri from 8am to 5pm.",
       action: "+1 (555) 000-0000",
     },
   ];
-    return (
-        <div>
-        <TbMessageCircle size="30" />
-        <TbMap2 size="30" />
-        <TbDeviceMobile size="30" />
-        <TbBrandFacebook size="30"/>
-        <TbBrandLinkedin size="30" />
-        <TbBrandX size="30" />
-        </div>
-    )
+
+  return (
+    <aside className="flex min-h-screen flex-col justify-between">
+      <div>
+        {contactInfo.map((info) => {
+          return (
+            <div key={info.id} className="mb-5">
+              <h3 className="text-lg font-bold">
+                {<info.icon size={25} />} {info.title}
+              </h3>
+              <p className="mb-2 text-slate-700">{info.description}</p>
+              <p className="cursor-pointer">{info.action}</p>
+            </div>
+          );
+        })}
+      </div>
+      <div className="flex gap-2">
+        <TbBrandFacebook size={25} className="cursor-pointer" />
+        <TbBrandLinkedin size={25} className="cursor-pointer" />
+        <TbBrandX size={25} className="cursor-pointer" />
+      </div>
+    </aside>
+  );
 }
 
 export default Aside;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,10 +7,10 @@ function Home() {
     <div className=" min-h-screen W-full bg-zinc-50 px-4 py-2 md:px-8 md:py-4">
       <Header />
       <div className="grid grid-cols-8">
-        <div className="col-span-2 hidden  bg-red-100 md:block">
+        <div className="col-span-2 hidden  md:block">
             <Aside />
         </div>
-        <div className="md:col-span-6 bg-green-100 col-span-full">
+        <div className="md:col-span-6 col-span-full">
             <Form />
         </div>
       </div>
