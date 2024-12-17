4393f2602631899368bf316843a031f7debeb4f9
DeepakShinde123 <Deepakshinde123@proton.me>
2024-12-17 08:21:51 +0000

    response gettting submitted

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -45,11 +45,12 @@ function Form() {
   return (
     <>
       <Intro />
-      <form className="flex flex-col gap-1" onSubmit={handleSubmit}>
+      <form className="flex flex-col gap-1" action="https://docs.google.com/forms/d/e/1FAIpQLSfRw81EJNBQSVmJTpMxG_sWtqNnrDSdDiHtjc27QU5oUiwQog/formResponse">
+      action={import.meta.env.SUBMIT_URL}
         {/* Input */}
         <input
           type="text"
-          name="fullname"
+          name={import.meta.env.VITE_NAME_FIELD}
           id="fullname"
           placeholder="Your name"
           className="border-b border-stone-700 bg-zinc-50 p-2 placeholder-slate-700 md:bg-lime-400"
@@ -59,7 +60,7 @@ function Form() {
         />
         <input
           type="email"
-          name="email"
+          name={import.meta.env.VITE_MESSAGE_FIELD}
           id="email"
           placeholder="your@company.com"
           className="border-b border-stone-700 bg-zinc-50 p-2 placeholder-slate-700 md:bg-lime-400"
@@ -69,7 +70,7 @@ function Form() {
         />
         <input
           type="text"
-          name="message"
+          name={import.meta.env.VITE_SERVICES_FIELD}
           id="message"
           placeholder="Tell us a bit about your project..."
           className="h-24 border-b border-stone-700 bg-zinc-50 p-2 placeholder-slate-700 md:bg-lime-400"
@@ -90,7 +91,7 @@ function Form() {
               >
                 <input
                   type="checkbox"
-                  name={service}
+                  name={import.meta.env.VITE_EMAIL_FIELD}
                   className="size-6"
                   onChange={(e) => handleCheckbox(service, e.target.checked)}
                 />
